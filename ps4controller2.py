5f8f07d5cd43fd5be76c5c946c335b93e2db06d1
yoji2 <yojiyamauchi3375@gmail.com>
2021-05-07 11:10:28 +0900

    pygame

---
+++ ps4controller2.py
@@ -0,0 +1,61 @@
+import pygame
+
+'''
+0 = SQUARE
+1 = X
+2 = CIRCLE
+3 = TRIANGLE
+4 = L1
+5 = R1
+6 = L2
+7 = R2
+8 = SHARE
+9 = OPTIONS
+10 = LEFT ANALOG PRESS
+11 = RIGHT ANALOG PRESS
+12 = PS4 ON BUTTON
+13 = TOUCHPAD PRESS
+'''
+
+pygame.init()
+
+j = pygame.joystick.Joystick(0)
+screen=pygame.display.set_mode((640,480))
+j.init()
+pygame.display.set_caption("MGV2 status")
+try:
+    while True:
+        events = pygame.event.get()
+        for event in events:
+            
+            if event.type == pygame.JOYBUTTONDOWN:
+                if j.get_button(0):
+                    print('SQUAE')
+                elif j.get_button(1):
+                    print('X')
+                elif j.get_button(2):
+                    print('Circl')
+                elif j.get_button(3):
+                    print('Triagnel')
+                elif j.get_button(4):
+                    print('L1')
+                else:
+                    print('other')
+                
+                screen.fill((0,0,0))
+                forward_joystickL = int(j.get_axis(1)*100) # left forward/back
+                font = pygame.font.SysFont("hg明朝ehgp明朝ehgs明朝e", 18)
+                strings=font.render(str(forward_joystickL),True, (255,255,255))
+                screen.blit(strings,(10,10))
+                pygame.display.update() 
+                forward_joystickR = int(j.get_axis(5)*100)
+                side_joystickL    = int(j.get_axis(0)*100)
+                side_joystickR    = int(j.get_axis(2)*100) #Right side by side
+                print(side_joystickL,' ',side_joystickR,' ', forward_joystickL, ' ' , forward_joystickR)
+            elif event.type == pygame.JOYBUTTONUP:
+                print("Button Released")
+            
+
+except KeyboardInterrupt:
+    print("EXITING NOW")
+    j.quit()
